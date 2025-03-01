b6d578078b60e5747864af568060040b7efbd6dd
Devanshu Roy <devanshu20000@gmail.com>
2025-03-01 18:15:57 +0530

    Fixed TrainCard component.

--- src/components/TrainCard/TrainCard.jsx
+++ src/components/TrainCard/TrainCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "./Traincard.module.css";
+import styled from "./TrainCard.module.css";
 import { useNavigate } from "react-router-dom";
 
 const Traincard = ({ train, date }) => {
